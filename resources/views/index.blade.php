f83e03f8318f1ca9432512dc3873f61513a5229d
yatanar <yatanarhtun.ytnh@gmail.com>
2021-08-02 17:57:03 +0900

    First Commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Yatanar's Book shop</title>
+</head>
+<body>
+<h1>Yatanar's Book Shop</h1>
+<label for="email">Enter your email:</label>
+<input type="email" id="email" name="email">
+
+<div>
+    <label for="pass">Password (8 characters minimum):</label>
+    <input type="password" id="pass" name="password"
+           minlength="8" required>
+</div>
+</body>
+</html>
